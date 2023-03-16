17317c49f0a5bd965b80182d5bd9589e22526d82
xen-py <austinkabanda@gmail.com>
2023-03-15 23:41:50 -0600

    Index Mostly finished and gallery just started

---
+++ public/admin/admin.js
@@ -0,0 +1,26 @@
+let imageAmount = null;
+
+
+/**
+ * adds and image to the gallery in any location
+ * pre:
+ * post: image has been added to array at location
+ */
+function addPhoto(image, position){
+
+    imgArray.add(image)
+
+}
+
+/**
+ * This function is called by the admin site and clears any images in the gallery
+ * pre: none
+ * post: images have been cleared
+ */
+function clearGallery (){
+
+}
+
+function downloadGallery(){
+
+}
